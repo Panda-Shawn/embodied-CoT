1063cbf55ae560c8b30ec7f19e7f1a0587757adc
Panda-Shawn <1019005866@qq.com>
2025-02-06 17:03:03 +0800

    requery incomplete episode

--- scripts/generate_embodied_data/merge_partial_reasonings.py
+++ scripts/generate_embodied_data/merge_partial_reasonings.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 
 
 if __name__=="__main__":
-    dataset_name = "libero_goal_no_noops" # "libero_10_no_noops"
+    dataset_name = "libero_object_no_noops" # "libero_10_no_noops"
     cat = dataset_name.split("_")[1]
     ds = tfds.load(dataset_name, data_dir="/home/nus/libero_new", split=f"train[{0}%:{100}%]")
     print(f"data size: {len(ds)}")
